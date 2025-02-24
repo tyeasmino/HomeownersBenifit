b3e0c54587dff549e739ee14d7386cd553288d4e
Tamima Yeasmin <tyeasmino@gmail.com>
2025-02-25 03:45:16 +0600

    updated for deploy

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from '../pages/Home'
 import ThankYou from '../pages/ThankYou'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 const Index = () => {
