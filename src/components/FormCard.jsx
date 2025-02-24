40adeb218d5d39656918fe53873b50e1a1848193
Tamima Yeasmin <tyeasmino@gmail.com>
2025-02-25 04:09:31 +0600

    form updating

--- src/components/FormCard.jsx
+++ src/components/FormCard.jsx
@@ -8,6 +8,11 @@ const FormCard = () => {
         homeOwner: "",
         monthlyBill: "",
         creditScore: "",
+        name: "",
+        street: "",
+        city: "",
+        zip: "",
+        state: "",
     })
 
     const nextStep = () => {
@@ -96,7 +101,7 @@ const FormCard = () => {
                         <div className='flex gap-20 justify-between'>
                             <div className='text-[10px] flex flex-col relative items-center text-blue-600'>
                                 <span className='w-5 h-5 flex items-center justify-center rounded-full border'>1</span>
-                                <p className=' '>Next</p> 
+                                <p className=' '>Next</p>
                             </div>
 
                             <div className='flex gap-12'>
@@ -147,12 +152,175 @@ const FormCard = () => {
                     </div>
                 )}
 
-                {step > 1 && <button onClick={prevStep}>Back</button>}
-                {step < 2 && <button onClick={nextStep}>Next</button>}
-                {step === 2 && <button onClick={() => alert("Submit FOrm")}>Submit</button>}
-                
+                {step === 3 && (
+                    <div>
+                        <div className='flex gap-20 justify-between'>
+                            <div className='text-[10px] flex flex-col relative items-center text-blue-600'>
+                                <span className='w-5 h-5 flex items-center justify-center rounded-full border'>1</span>
+                                <p className=' '>Next</p>
+                            </div>
+
+                            <div className='flex gap-12'>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-green-500'>2</span>
+                                    <p className=''>Next</p>
+                                    <span className='absolute top-3 left-10 w-[30px] bg-gray-500 h-[1px]'></span>
+                                </div>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-gray-500'>3</span>
+                                    <p className=''>Next</p>
+                                    <span className='absolute top-3 left-10 w-[30px] bg-gray-500 h-[1px]'></span>
+                                </div>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-gray-500'>4</span>
+                                    <p className=''>Next</p>
+                                    <span className='absolute top-3 left-10 w-[30px] bg-gray-500 h-[1px]'></span>
+                                </div>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-gray-500'>5</span>
+                                    <p className=''>Last Page</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-5'>
+                            <h5 className='font-bold text-[20px]'>Your estimated credit score? *</h5>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="creditScore"
+                                    value="More Than 740 (Excellent)"
+                                    checked={formData.creditScore === "More Than 740 (Excellent)"}
+                                    onChange={handleChange}
+                                />{" "}
+                                More Than 740 (Excellent)
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="creditScore"
+                                    value="680 739 (Good)"
+                                    checked={formData.creditScore === "680 739 (Good)"}
+                                    onChange={handleChange}
+                                />{" "}
+                                680 739 (Good)
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="creditScore"
+                                    value="Less Than 569 (Poor)"
+                                    checked={formData.creditScore === "Less Than 569 (Poor)"}
+                                    onChange={handleChange}
+                                />{" "}
+                                Less Than 569 (Poor)
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="creditScore"
+                                    value="I'm not sure"
+                                    checked={formData.creditScore === "I'm not sure"}
+                                    onChange={handleChange}
+                                />{" "}
+                                I'm not sure
+                            </label>
+                        </div>
+                    </div>
+                )}
+
+
+                {step === 4 && (
+                    <div>
+                        <div className='flex gap-20 justify-between'>
+                            <div className='text-[10px] flex flex-col relative items-center text-blue-600'>
+                                <span className='w-5 h-5 flex items-center justify-center rounded-full border'>1</span>
+                                <p className=' '>Next</p>
+                            </div>
+
+                            <div className='flex gap-12'>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-green-500'>2</span>
+                                    <p className=''>Next</p>
+                                    <span className='absolute top-3 left-10 w-[30px] bg-gray-500 h-[1px]'></span>
+                                </div>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-gray-500'>3</span>
+                                    <p className=''>Next</p>
+                                    <span className='absolute top-3 left-10 w-[30px] bg-gray-500 h-[1px]'></span>
+                                </div>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-gray-500'>4</span>
+                                    <p className=''>Next</p>
+                                    <span className='absolute top-3 left-10 w-[30px] bg-gray-500 h-[1px]'></span>
+                                </div>
+                                <div className='text-[10px] flex flex-col relative items-center mx-2'>
+                                    <span className='w-5 h-5 flex items-center justify-center  rounded-full border border-gray-500'>5</span>
+                                    <p className=''>Last Page</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-col gap-5'>
+                            <h5 className='font-bold text-[20px]'>Your Name *</h5>
+                            <input
+                                className='border  border-gray-700 rounded-full px-5 py-2'
+                                type="text"
+                                name="name"
+                                placeholder='Type Your Name'
+                                value={formData.name}
+                                onChange={handleChange}
+                                />{" "}
+
+
+                            <h5 className='font-bold text-[20px]'>Address *</h5>
+                            <input
+                                className='border  border-gray-700 rounded-full px-5 py-2'
+                                type="text"
+                                name="address"
+                                placeholder='Street Address'
+                                value={formData.address}
+                                onChange={handleChange}
+                            />{" "}
+
+                            <input
+                                className='border  border-gray-700 rounded-full px-5 py-2'
+                                type="text"
+                                name="city"
+                                placeholder='City'
+                                value={formData.city}
+                                onChange={handleChange}
+                            />{" "}
+
+                            <input
+                                className='border  border-gray-700 rounded-full px-5 py-2'
+                                type="text"
+                                name="zip"
+                                placeholder='Zip'
+                                value={formData.zip}
+                                onChange={handleChange}
+                            />{" "}
+
+                            <input 
+                                className='border  border-gray-700 rounded-full px-5 py-2'
+                                type="text"
+                                name="state"
+                                placeholder='State'
+                                value={formData.state}
+                                onChange={handleChange}
+                            />{" "}                           
+                        </div>
+                    </div>
+                )}
+
+                <div className='w-full flex justify-center gap-5'>
+                    {step > 1 && <button onClick={prevStep} style={{ fontFamily: "'Space Grotesk', sans-serif" }} className=' hover:bg-[#033E8A] text-[#033E8A] border border-[#033E8A] transition-all duration-300 ease-in-out font-semibold text-sm hover:text-white px-10 py-3.5 rounded-full' >Back</button>}
+                    {step < 5 && <button onClick={nextStep} style={{ fontFamily: "'Space Grotesk', sans-serif" }} className=' bg-[#033E8A] hover:bg-white hover:text-[#033E8A] border hover:border-[#033E8A] transition-all duration-300 ease-in-out font-semibold text-sm text-white px-28 py-3.5 rounded-full' >Next</button>}
+                    {step === 5 && <button onClick={() => alert("Submit FOrm")}>Submit</button>}
+                </div>
+
+
                 {/* <div className='text-center my-7'>
-                    <Link to='' style={{ fontFamily: "'Space Grotesk', sans-serif" }} className='w-[200px] bg-[#033E8A] hover:bg-white hover:text-[#033E8A] hover:border hover:border-[#033E8A] transition-all duration-300 ease-in-out font-semibold text-sm text-white px-36 py-3.5 rounded-full'>Next</Link>
+                    <Link to='' >Next</Link>
                 </div> */}
             </div>
         </section>
